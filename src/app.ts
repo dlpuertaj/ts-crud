10a5db43c479b350548c742ebcfc2bb301bee038
David Leonardo <dlpuerta@gmail.com>
2024-09-24 01:16:30 -0500

    First commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, {Application, Request, Response} from 'express';
+
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+res.send('Welcome to my first Express TypeScript project');
+});
+
+app.listen(PORT, ()=>{
+console.log('Server is running on http://localhost:${PORT}');
+});
